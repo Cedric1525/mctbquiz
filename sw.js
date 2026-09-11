6c8b4431511a1d0059ca6ba01212f30162371689
Cedric1525 <cedric.sjc1525@gmail.com>
2026-09-11 14:26:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker : rend l'application utilisable hors connexion.
    Changez CACHE a chaque mise a jour du contenu pour forcer le rafraichissement. */
-const CACHE = "mctb-quiz-v7";
+const CACHE = "mctb-quiz-v9";
 const BASE = [
   "./", "./index.html", "./questionnaires.json", "./manifest.json",
   "./icone-192.png", "./icone-512.png"
